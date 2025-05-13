2dd293f8abfe0ef4f42010b5495314f5576cf5f8
moraa-dev <a.moraa@iscodkenya.org>
2025-05-13 17:56:12 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from pet import Pet
+
+# Create a pet instance
+my_pet = Pet("Buddy")
+
+# Interact with the pet
+my_pet.get_status()
+my_pet.eat()
+my_pet.sleep()
+my_pet.play()
+my_pet.train("sit")
+my_pet.train("roll over")
+my_pet.get_status()
+my_pet.show_tricks()
